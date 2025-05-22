0e540c124b723468f76f6e17212135980c7691ef
Nick Pendery <npendery@gmail.com>
2025-05-21 21:28:46 -0600

    Use a function to create the application

--- module_1/run.py
+++ module_1/run.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from pages.routes import pages_bp
 
-app = Flask(__name__)
-app.register_blueprint(pages_bp)
+def create_app():
+    """
+    Creates the application and registers the blueprints.
+    """
+    app = Flask(__name__)
+    app.register_blueprint(pages_bp)
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host='0.0.0.0', port=8000) 
