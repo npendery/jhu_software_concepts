207824ce6d05d2e17c750107e2e26397b5e57ac2
Nick Pendery <npendery@gmail.com>
2025-06-21 13:03:40 -0600

    Copy over code from module 1

---
+++ module_6/run.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from pages.routes import pages_bp
+
+app = Flask(__name__)
+app.register_blueprint(pages_bp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
